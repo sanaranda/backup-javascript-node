17ecd54aced5cd3e78a1fb123ee9d1861ffd35a4
sanaranda <santiagoaranda@gmail.com>
2020-08-11 17:00:27 -0300

    cambio app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let listadoDeComandos = 
 ['Comando desconocido, este es el listado de comandos: ',
-'listar', 
+'listar titulos',
+'listar todo', 
 'filtrar pendiente' ,
 'filtrar en_progreso' , 
 'filtrar terminada',
@@ -13,25 +14,31 @@ let comando2 = process.argv[3]; // segundo argumento o comando
 let comando3 = process.argv[4]; // tercer argumento o comando
 let tareas = funciones.leerJSON(); // leo arhivo Json y lo dejo en formato objeto, o sea, lo Parseo
 
-
 function Tarea (titulo, estado, descripcion){ // funcion constructora de objetos, se usa Mayuscula para el nombre del tipo de objeto creado
     this.titulo = titulo;
     this.estado = estado;
     this.descripcion = descripcion;
     };
 
-
-
-switch (comando1) {
+    switch (comando1) {   // veo cual es el primer comando
     case 'listar':
-                                   
-        tareas.forEach((elemento,i) => {    //se puede pasar el index como segundo parametro en forEach
-            console.log("Titulo tarea numero " + (i+1) + " = " + elemento.titulo)
-        });
+        if(((comando2 == 'titulos') || (comando2 == 'todo')) && (typeof comando2 != 'undefined')){ // valido si el segudo comando esta correcto y sino aviso
+            switch (comando2) {   
+            case 'titulos':
+                tareas.forEach((elemento,i) => {    //se puede pasar el index como segundo parametro en forEach
+                console.log("Titulo " + (i+1) + " = " + elemento.titulo)});
+            break;
+            case 'todo':
+                tareas.forEach((elemento,i) => {    
+                console.log("Titulo " + (i+1) + " = " + elemento.titulo + " / Descripcion " + (i+1) + " = " + elemento.descripcion + " / Estado " + (i+1) + " = " + elemento.estado)});
+            break;
+            }
     break;
+        }else {console.log('Falto segundo comando');}
+    break;  
     
     case 'filtrar':                                                    // si en vez de poner listar pongo filtrar y luego el estado, me lista todas las tareas con ese estado
-        if((comando2 != 'pendiente') && (comando2 != 'en_progreso') && (comando2 != 'terminada') && (typeof comando2 != 'undefined')){
+        if((comando2 != 'pendiente') && (comando2 != 'en curso') && (comando2 != 'terminada') && (typeof comando2 != 'undefined')){
         console.log("Verificar como escribiste el estado por favor"); //avisa que algo se escribio mal , pero si no se escribio nada pasa el if de abajo
         break
         }
@@ -49,25 +56,18 @@ switch (comando1) {
 
     case 'crear':
         if((typeof comando2 != 'undefined') && (typeof comando3 != 'undefined')){
-       
-        let nuevaTarea = new Tarea (comando2, "pendiente", comando3 ); // hacer objeto tarea nuevo
-        tareas.push(nuevaTarea);                                       // meterla en el array de tareas                                         
-        let tareasJson = JSON.stringify(tareas);                       // pasar el array de tareas a formato Json
-        funciones.escribirJSON(tareasJson);                            // ahora que esta en Json, pisar el archivo tareas.json viejo
+            let nuevaTarea = new Tarea (comando2, "pendiente", comando3 ); // hacer objeto tarea nuevo
+            tareas.push(nuevaTarea);                                       // meterla en el array de tareas                                         
+            let tareasJson = JSON.stringify(tareas);                       // pasar el array de tareas a formato Json
+            funciones.escribirJSON(tareasJson);
+            console.log('Tarea agregada!');                            // ahora que esta en Json, pisar el archivo tareas.json viejo
         }else{
             console.log("Falto Titulo y/o Descripcion");
         };                        
-
         break;
+            
     default:
         console.log(listadoDeComandos) // si no se puso un comando conocido, le doy la lista de los comandos actuales
         
         break;
-}
-
-// if (accion === 'listar'){
-//     console.log('Listando las tareas');
-// }else{
-//     console.log('Listado de acciones:');
-//     console.log('...listar');
-// }
+    }
